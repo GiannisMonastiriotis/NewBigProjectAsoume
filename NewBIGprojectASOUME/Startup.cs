79dbb11a75aeaa7eee13abc9989a471b680ffe2b
Giannis Monastiriotis <ioannismonastiriotis@gmail.com>
2022-06-25 14:08:14 +0300

    Add project files.

---
+++ NewBIGprojectASOUME/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NewBIGprojectASOUME.Startup))]
+namespace NewBIGprojectASOUME
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
